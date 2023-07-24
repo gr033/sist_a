a1207fd09c3b742644ca842a4dcb93658266b79e
gr033 <gr033@naver.com>
2023-07-24 11:41:05 +0900

    LoginTest complete

--- LoginTest.java
+++ LoginTest.java
@@ -1,9 +1,11 @@
 class LoginTest{
-	public boolean isMember(String id, String pwd){
-		if(id.equals("hyeonju") && pwd.equals("Hello")){
-			return true;
-		}else{
-			return false;
+	public int isMember(String id, String pwd){
+		int re = -1;
+		return re;
+		if(id.equals("도비") && pwd.equals("집요정")){
+			re = 1;
+			System.out.println("도비는 자유에요. 자유인거에요. \n저도 자유롭고 싶어요. 집갈래요.");
+			return re;
 		}
 	}
 }
